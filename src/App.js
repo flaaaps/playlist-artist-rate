ecda43387a9ef3ef434e6cfa1ae92b71e017b9e0
flaaaps <julianwening4@gmail.com>
2021-01-10 16:41:30 +0100

    :sparkles: Support custom playlists

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@
     to run correctly
 */
 
+import { generateSpotifyAccessToken } from './api';
 import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
     const [tracks, setTracks] = useState([]);
+    const [playlistId, setPlaylistId] = useState('6EThH5r1x2ALtpQIaCaeKi');
+    const [value, setValue] = useState('');
     const [accessToken, setAccessToken] = useState(null);
+
+    const artists = ['Ava Max', 'Machine Gun Kelly'];
 
     useEffect(() => {
         const fetchData = async () => {
             const getAccessToken = async () => {
-                const { access_token } = await (await fetch('http://localhost:1237')).json();
+                const { access_token } = await generateSpotifyAccessToken();
                 setAccessToken(access_token);
             };
-            console.log(!accessToken);
+
             if (!accessToken) return await getAccessToken();
-            fetch('https://api.spotify.com/v1/playlists/5yACIst2lkJRtgMLBOqcJK', {
+            fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
                 headers: {
                     Authorization: 'Bearer ' + accessToken,
                 },
@@ -36,7 +42,7 @@ function App() {
         };
 
         fetchData();
-    }, [accessToken]);
+    }, [accessToken, playlistId]);
 
     const countArtist = (name) => {
         let count = 0;
@@ -62,36 +68,58 @@ function App() {
         });
         return songs;
     };
+
+    const handleInputChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {
+            setPlaylistId(validateInput(value));
+        }
+    };
+
+    const validateInput = (input) => {
+        let playlistId;
+        if (input.indexOf('http') > -1) {
+            let splitted = input.split('/');
+            const id = splitted[splitted.length - 1];
+            // remove possible query params
+            splitted = id.split('?').reverse();
+            playlistId = splitted[splitted.length - 1];
+        } else if (input.indexOf('spotify:playlist:') > -1) {
+            const splitted = input.split(':');
+            playlistId = splitted[splitted.length - 1];
+        } else {
+            playlistId = input;
+        }
+
+        return playlistId;
+    };
+
     return (
         <div className="App">
             <h1>Ava Max x MGK Rate Calculator</h1>
+
+            <input placeholder="Enter a spotify playlist URI, URL or ID" value={value} onKeyDown={handleKeyPress} onChange={handleInputChange} />
             <p>Ava Max contributions: {countArtist('Ava Max')}</p>
             <p>Machine Gun Kelly contributions: {countArtist('Machine Gun Kelly')}</p>
             <p>Quota/Rate: {artistRate(countArtist('Ava Max'), countArtist('Machine Gun Kelly'))}</p>
-            <div>
-                <h3>Ava Max</h3>
-                <ol>
-                    {artistSongs('Ava Max').map((song, i) => {
-                        return (
-                            <li key={i}>
-                                {song.name} by {new Intl.ListFormat('en').format(song.artists.map((artist) => artist?.name))}
-                            </li>
-                        );
-                    })}
-                </ol>
-            </div>
-            <div>
-                <h3>MGK</h3>
-                <ol>
-                    {artistSongs('Machine Gun Kelly').map((song, i) => {
-                        return (
-                            <li key={i}>
-                                {song.name} by {new Intl.ListFormat('en').format(song.artists.map((artist) => artist?.name))}
-                            </li>
-                        );
-                    })}
-                </ol>
-            </div>
+            {artists.map((artist, i) => (
+                <div key={i}>
+                    <h3>{artist}</h3>
+                    {artistSongs(artist).length === 0 && <p>No songs found.</p>}
+                    <ol>
+                        {artistSongs(artist).map((song, i) => {
+                            return (
+                                <li key={i}>
+                                    {song.name} by {new Intl.ListFormat('en').format(song.artists.map((artist) => artist?.name))}
+                                </li>
+                            );
+                        })}
+                    </ol>
+                </div>
+            ))}
         </div>
     );
 }
